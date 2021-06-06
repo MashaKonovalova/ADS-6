fe22bd7a57793761614019834570ffcccb5cadb8
masha konovalova <75724191+MashaKonovalova@users.noreply.github.com>
2021-06-06 18:11:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,25 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string>btree;
+    std::string Word = "";
+    char lowerletter = 'a' - 'A';
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return btree;
+    }
+    while (!file.eof()) {
+        char ch = file.get();
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            if (ch >= 'A' && ch <= 'Z')
+                ch += lowerletter;
+            Word += ch;
+        } else if (Word != "") {
+            btree.add(Word);
+            Word = "";
+        }
+    }
+    file.close();
+    return btree;
 }
